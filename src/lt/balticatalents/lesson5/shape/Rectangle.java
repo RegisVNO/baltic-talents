2fcd3693ca283f603bc31ea7e082da04f7886b05
Marek <S1164B@corp1.ad1.seb.net>
2018-06-13 20:03:37 +0300

    lesson 5

---
+++ src/lt/balticatalents/lesson5/shape/Rectangle.java
@@ -0,0 +1,37 @@
+package lt.balticatalents.lesson5.shape;
+
+class Rectangle extends Shape {
+    private double x;
+    private double y;
+
+    Rectangle(double x, double y) {
+        if (x <= 0 || y <= 0) {
+            System.out.println("\nInvalid values!");
+            System.out.println("Assigning default values... [1, 1]");
+            this.x = 1;
+            this.y = 1;
+        } else {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    Rectangle() {
+        this.x = 1;
+        this.y = 1;
+    }
+
+    @Override
+    double area() {
+        return x * y;
+    }
+
+    @Override
+    double perimeter() {
+        return 2 * (x + y);
+    }
+
+    boolean isSquare() {
+        return x == y;
+    }
+}
---
+++ src/lt/balticatalents/lesson5/shape/Shape.java
@@ -0,0 +1,7 @@
+package lt.balticatalents.lesson5.shape;
+
+abstract class Shape {
+    abstract double area();
+
+    abstract double perimeter();
+}
